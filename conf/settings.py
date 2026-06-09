d06552e6473c5012e24c27b4dfc4d6429bd47b7f
Hojiakbar Hayrullayev <usa20070302@gmail.com>
2026-06-09 20:19:54 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -176,5 +176,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'usa20070302@gmail.com'
-EMAIL_HOST_PASSWORD = 'hvpj hgle xnwm vptz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
